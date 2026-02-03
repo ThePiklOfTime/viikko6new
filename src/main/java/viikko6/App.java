b3d57f77fbc63ccab62d5885332a0dc333a78c0b
ThePiklOfTime <nicotolvanen@gmail.com>
2026-02-03 21:08:05 +0200

    Added all functionality to menu

--- src/main/java/viikko6/App.java
+++ src/main/java/viikko6/App.java
@@ -1,5 +1,10 @@
 package viikko6;
 
+import java.lang.reflect.Array;
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.HashMap;
+
 /**
  * Hello world!
  *
@@ -8,6 +13,79 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        Scanner scanner = new Scanner(System.in);
+        boolean exit = false;
+        ArrayList<Instrument> instruments = new ArrayList<>();
+    
+        while (!exit) {
+            System.out.println("1) Lisää soitin \n2) Listaa soittimet\n3) Viritä kielisoittimet\n4) Soita rumpuja\n0) Lopeta ohjelma");
+            
+            if(scanner.hasNext()){
+                int i = Integer.parseInt(scanner.nextLine());
+                
+            switch (i) {
+                case 1:
+                    System.out.println("Minkä soittimen haluat lisätä? 1) Kitara 2) Viulu 3) Rummut");
+                    if(scanner.hasNext()){
+                        int j = Integer.parseInt(scanner.nextLine());
+                        System.out.println("Anna valmistajan nimi:");
+                        String manufacturer = scanner.nextLine();
+                        System.out.println("Anna hinta euroina:");
+                        int price = Integer.parseInt(scanner.nextLine());
+                        switch (j) {
+                            case 1:
+                                instruments.add(new Guitar(manufacturer, price));
+                                break;
+                            case 2:
+                                instruments.add(new Violin(manufacturer, price));
+                                break;
+                            case 3:
+                                instruments.add(new Drum(manufacturer, price));
+                                break;
+                            default:
+                                System.out.println("Syöte oli väärä");
+                                break;
+                        }
+                    }       
+                    break;
+                case 2:
+                    if (instruments.isEmpty())  {
+                        System.out.println("Soittimia ei ole lisätty.");
+                        break;
+                    } 
+                    for (Instrument instrument : instruments) {
+                        System.out.println(instrument.getDetails());
+                    }
+                    break;
+                case 3:
+                    for (Instrument instrument : instruments) {
+                        if (instrument instanceof StringInstrument) {
+                            StringInstrument stringInstrument = (StringInstrument) instrument;
+                            stringInstrument.tune();
+                        }
+                    }
+                    break;
+                case 4:
+                    for (Instrument instrument : instruments) {
+                        if (instrument instanceof Drum) {
+                            Drum drum = (Drum) instrument;
+                            drum.playBeat();
+                        }
+                    }
+                    break;
+                case 0:
+                    exit = true;
+                    break;
+                
+                default:
+                    System.out.println("Syöte oli väärä");
+                    break;
+            
+            
+                }
+            }
+        }
+        System.out.println("Kiitos ohjelman käytöstä.");
+        scanner.close();
     }
 }
